060089fcc2af13f0e81e3d4ae15ebb2eb8ed13bb
jmunozro <jesmunrod@gmail.com>
2021-05-10 23:00:35 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const csvFilePathArr = [
-  //"/Users/jesusmunoz/Desktop/workspace/jesus/node-alerts/src/aeat-calculator/2021mov.csv",
+  "/Users/jesusmunoz/Desktop/workspace/jesus/node-alerts/src/aeat-calculator/2021mov.csv",
   "/Users/jesusmunoz/Desktop/workspace/jesus/node-alerts/src/aeat-calculator/2020ibkr.csv",
   "/Users/jesusmunoz/Desktop/workspace/jesus/node-alerts/src/aeat-calculator/2020revolut.csv",
   //"/Users/jesusmunoz/Desktop/workspace/jesus/node-alerts/src/aeat-calculator/extra2020.csv",
@@ -13,6 +13,6 @@ const csvFilePathDivArr = [
 module.exports = {
   csvFilePathDivArr,
   csvFilePathArr,
-  startDate: "20200000",
-  endDate: "20210000"
+  startDate: "20210000",
+  endDate: "20220000"
 };
